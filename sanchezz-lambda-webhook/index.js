8130d8318efe80e729e0e5ce6f1c572518b9ae4c
Emmanuel <sanchezz985@gmail.com>
2021-03-18 17:32:52 -0600

    se agrega log

--- sanchezz-lambda-webhook/index.js
+++ sanchezz-lambda-webhook/index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    // TODO implement
+    console.log(event);
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
