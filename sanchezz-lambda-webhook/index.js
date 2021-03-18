49157972a30a345ef85e58659cf258c948502689
Emmanuel <sanchezz985@gmail.com>
2021-03-18 17:53:41 -0600

    Se corrige issue

--- sanchezz-lambda-webhook/index.js
+++ sanchezz-lambda-webhook/index.js
@@ -1,7 +1,8 @@
 exports.handler = async (event) => {
+    console.log(event.body);
     const body = event.body;
     try {
-        console.log(`Deploying for commit ${body.commits.id}`);
+        console.log(`Deploying for commit ${body.commits[0].id}`);
     }catch (error){
         console.log(`Error while deploying ${error}`);
     }
