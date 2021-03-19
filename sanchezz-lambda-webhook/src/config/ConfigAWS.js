36d66be7690dd2e76622ffa02c9d9d9a50b533c9
Emmanuel <sanchezz985@gmail.com>
2021-03-19 17:08:22 -0600

    functionName: sanchezz-lambda-database; environment: dev; description: Subir a S3;

--- sanchezz-lambda-webhook/src/config/ConfigAWS.js
+++ sanchezz-lambda-webhook/src/config/ConfigAWS.js
@@ -1,6 +1,6 @@
 // Importing internal libraries
 const AWSLambda = require('../services/AwsLambda');
-const AWSS3 = require('../utilities/AWSS3');
+const AWSS3 = require('../services/AWSS3');
 
 exports.AWSLambda = AWSLambda;
 exports.AWSS3 = AWSS3;
