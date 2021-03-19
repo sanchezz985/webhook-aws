4b061f04ba077cd7b290d5837e87df0f8f5c00ac
Emmanuel <sanchezz985@gmail.com>
2021-03-18 18:13:14 -0600

    functionName: sanchezz-lambda-webhook;environment: dev;description: Se agrega plantilla;

--- sanchezz-lambda-webhook/index.js
+++ sanchezz-lambda-webhook/index.js
@@ -2,6 +2,9 @@ exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     try {
         console.log(`=== Deploying for commit ${body.commits[0].id} ===`);
+        if(!body.commits[0].message)
+            throw new Error("Commit without correct template");
+
         const commitMessage = body.commits[0].message.slit(";");
         const functions = commitMessage[0];
         const environments = commitMessage[1];
