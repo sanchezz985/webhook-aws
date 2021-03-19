4c4793d01f659c8c4624f30f62fa30623c873fe1
Emmanuel <sanchezz985@gmail.com>
2021-03-18 18:10:37 -0600

    functionName: sanchezz-lambda-webhook;environment: dev;description: Se agrega plantilla;

--- sanchezz-lambda-webhook/index.js
+++ sanchezz-lambda-webhook/index.js
@@ -1,7 +1,14 @@
 exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     try {
-        console.log(`Deploying for commit ${body.commits[0].id}`);
+        console.log(`=== Deploying for commit ${body.commits[0].id} ===`);
+        const commitMessage = body.commits[0].message.slit(";");
+        const functions = commitMessage[0];
+        const environments = commitMessage[1];
+        const message = commitMessage[2];
+        console.log("Deploying info: ");
+        console.log(`Functions to deploy: ${functions}\n Environments: ${environments} \n Message: ${message}`);        
+
     }catch (error){
         console.log(`Error while deploying ${error}`);
     }
@@ -9,4 +16,4 @@ exports.handler = async (event) => {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
     }; 
-}; 
+};
