892b52e0345b0c3d4451b1dac9f6be257dc30e9d
Emmanuel <sanchezz985@gmail.com>
2021-03-18 21:09:58 -0600

    functionName: sanchezz-lambda-webhook;environment: dev;description: Obtener Lambdas de la cuenta

--- sanchezz-lambda-webhook/src/handlers/index.js
+++ sanchezz-lambda-webhook/src/handlers/index.js
@@ -15,6 +15,7 @@ exports.handler = async (event) => {
         console.log(`\n Deploying info:  \n Functions to deploy: ${functions}\n Environments: ${environments} \n Message: ${message}`);
 
         await DeployService.getFunctions();
+        
     }catch (error){
         console.log(`Error while deploying ${error}`);
     }
