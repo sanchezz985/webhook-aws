4b23bc1d60cc4cf59ddf00900374e6f0e4d21b53
Emmanuel <sanchezz985@gmail.com>
2021-03-18 19:17:20 -0600

    functionName: sanchezz-lambda-webhook;environment: dev;description: Obtener Lambdas de la cuenta

--- sanchezz-lambda-webhook/src/services/DeployService.js
+++ sanchezz-lambda-webhook/src/services/DeployService.js
@@ -9,14 +9,12 @@ const getFunctions = async () => {
     do {
         try {
             data = await lambda.listFunctions(data.NextMarker);
-            console.log(JSON.stringify(data));
+            console.log(data);
             functions = [...functions, ...data.Functions];
         } catch (e) {
-            console.log(e);
-            break;
+            throw e;
         }
-    } while (data.NextMarker);
-    
+    } while (data.NextMarker);    
     if(functions.length > 0) {
         fs.createWriteStream("lambda_functions.json", JSON.stringify(functions));
         console.log("File lambda_functions.json was created successfully")
