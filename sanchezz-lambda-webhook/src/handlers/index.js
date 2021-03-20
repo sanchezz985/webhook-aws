26cf0c643b719aec24e12a69188b522754612b9f
Emmanuel <sanchezz985@gmail.com>
2021-03-19 19:56:45 -0600

    functionName: sanchezz-lambda-database; description: Fix issue in dev;

--- sanchezz-lambda-webhook/src/handlers/index.js
+++ sanchezz-lambda-webhook/src/handlers/index.js
@@ -4,11 +4,11 @@ exports.handler = async (event) => {
     const body = JSON.parse(event.body);
     console.log(JSON.stringify(body));
     try {
-        console.log(`=== Deploying for commit ${body.commits[0].id} ===`);
-        if(!body.commits[0].message)
+        console.log(`=== Deploying for commit ${body.head_commit.id} ===`);
+        if(!body.head_commit.message)
             throw new Error("Commit without correct template");
 
-        const commitMessage = body.commits[0].message.trim().split(";");
+        const commitMessage = body.head_commit.message.trim().split(";");
         const functions = getLineValue(commitMessage[0]);
         const environments = getLineValue(commitMessage[1]);
         const message = getLineValue(commitMessage[2]);
