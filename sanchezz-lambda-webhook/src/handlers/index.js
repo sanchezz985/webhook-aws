953d79974670a191c663d0538c436dafd819c949
Emmanuel <sanchezz985@gmail.com>
2021-03-19 19:58:40 -0600

    functionName: sanchezz-lambda-database; description: Update lambda function in dev;

--- sanchezz-lambda-webhook/src/handlers/index.js
+++ sanchezz-lambda-webhook/src/handlers/index.js
@@ -22,7 +22,6 @@ exports.handler = async (event) => {
     }catch (error){
         console.log(`Error while deploying ${error}`);
     }
-    
     return {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
