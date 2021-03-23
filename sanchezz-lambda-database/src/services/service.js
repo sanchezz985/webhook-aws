d233c89e1ece8ced97616cba53d5a9c12d72424b
Emmanuel <sanchezz985@gmail.com>
2021-03-23 09:06:54 -0600

    functionName: sanchezz-lambda-database;\ndescription: Update lambda function in dev;

--- sanchezz-lambda-database/src/services/service.js
+++ sanchezz-lambda-database/src/services/service.js
@@ -1,12 +1,4 @@
 const DevMethod = () => {
-    // new changes in develop
-    // new changes 2
-    // new changes 3
-    // new changes 4
-    // new changes 5
-    // new changes 6
-    // new changes 7
-    // new changes 8
     console.log("DEVELOP");
 };
 
